ba1dac9ec35c7b90241561fe08ef062c425f606d
Nick Sutterer <apotonick@gmail.com>
2015-07-06 10:37:32 +1000

    use disposable's :default.

--- lib/tyrant/authenticatable.rb
+++ lib/tyrant/authenticatable.rb
@@ -2,32 +2,43 @@ require "disposable/twin/struct"
 
 module Tyrant
   class Authenticatable < Disposable::Twin
-    property :auth_meta_data do
+    feature Default
+
+    property :auth_meta_data, default: Hash.new do
       include Struct
       property :confirmation_token
+      property :confirmed_at
     end
 
-    def confirmable?(token=nil)
-      return false unless auth_meta_data # FIXME: handle this in Struct.
-      persisted_token = auth_meta_data.confirmation_token
+    module Confirm
+      # without token, this decides whether the user model can be activated (e.g. via "set a password").
+      # with token, this additionally tests if the token is correct.
+      def confirmable?(token=nil)
+        persisted_token = auth_meta_data.confirmation_token
 
-      # TODO: add expiry etc.
-      return false unless (persisted_token.is_a?(String) and persisted_token.size > 0)
+        # TODO: add expiry etc.
+        return false unless (persisted_token.is_a?(String) and persisted_token.size > 0)
 
-      return compare_token(token) if token
-      true
-    end
+        return compare_token(token) if token
+        true
+      end
 
-    # alias_method :confirmed?, :confirmable?
-    def confirmed?
-      return false unless auth_meta_data #FIXME, of course.
-      return true if auth_meta_data.has_key?(:confirmation_token)  and auth_meta_data[:confirmation_token].nil?
-      return false if auth_meta_data[:confirmation_token].size > 0
-    end
+      # alias_method :confirmed?, :confirmable?
+      def confirmed?
+        not auth_meta_data.confirmed_at.nil?
+      end
 
-  private
-    def compare_token(token)
-      token == auth_meta_data.confirmation_token
-    end
+      def confirm!(confirmed_at=DateTime.now)
+        auth_meta_data.confirmation_token = nil
+        auth_meta_data.confirmed_at       = confirmed_at # TODO: test optional arg.
+      end
+
+    private
+      def compare_token(token)
+        token == auth_meta_data.confirmation_token
+      end
+    end # Confirm
+    include Confirm
+
   end
 end
--- test/authenticatable_test.rb
+++ test/authenticatable_test.rb
@@ -16,6 +16,21 @@ class AuthenticatableTest < MiniTest::Spec
     it { Authenticatable.new(User.new({})).confirmable?("yo!").must_equal false }
     it { Authenticatable.new(User.new({confirmation_token: "yo!"})).confirmable?("yo!").must_equal true }
     # TODO: add expiry.
-    # todo: WITH token
+  end
+
+  describe "#confirmed?" do
+    # blank.
+    it { Authenticatable.new(User.new).confirmed?.must_equal false }
+    # with token.
+    it { Authenticatable.new(User.new({confirmation_token: "yo!"})).confirmed?.must_equal false }
+
+    it do
+      auth = Authenticatable.new(User.new({confirmation_token: "yo!"}))
+      auth.confirm!
+      # confirmed?
+      auth.confirmed?.must_equal true
+      # confirmed_at.
+      auth.auth_meta_data.confirmed_at.must_be_kind_of DateTime
+    end
   end
 end
