135863684760ed8dd2918630b5748f01927263b2
Nick Sutterer <apotonick@gmail.com>
2015-12-15 11:00:33 +1100

    add changes.

--- lib/tyrant/railtie.rb
+++ lib/tyrant/railtie.rb
@@ -3,13 +3,14 @@ module Tyrant
       require "warden"
 
       # DISCUSS: it will be configurable what user class etc. and might be moved to Ops.
+      # DISCUSS: this should actually use Tyrant::Session to log in and out user?
       config.app_middleware.use Warden::Manager do |config|
         Warden::Manager.serialize_into_session do |user|
           user.id
         end
 
         Warden::Manager.serialize_from_session do |id|
-          User.find_by(id: id)
+          User.find_by(id: id) # Session.sign_in!(user) or something!
         end
       end
   end
