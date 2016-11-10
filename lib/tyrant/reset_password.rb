923c15f9bfe835591ae6dbc9115ff9efe6f8b350
emasurf85 <emanuele.magliozzi@gmail.com>
2016-11-10 15:09:49 +1100

    It seems that using a normal Class it works. Needs to be replaced with a TRB operation

--- lib/tyrant/reset_password.rb
+++ lib/tyrant/reset_password.rb
@@ -1,14 +1,28 @@
 require 'securerandom'
+require "trailblazer/operation"
+require "trailblazer/operation/model"
+require "active_model"
+require "reform/form/validate"
+require "reform/form/active_model/validations"
 
 module Tyrant
   class ResetPassword
+    # include Model
+    # model User, :find
     
+    # def process(params)
+    #   new_authentication(model)
+    # end
+
+  # private
     def new_authentication(model)
+      puts model.inspect
       auth = Tyrant::Authenticatable.new(model)
       auth.digest!(generate_password) # contract.auth_meta_data.password_digest = ..
       auth.confirmed!
       auth.sync
       notify_user(model.email)
+      return model
     end
 
     def generate_password
--- test/reset_password_test.rb
+++ test/reset_password_test.rb
@@ -1,5 +1,20 @@
 require "test_helper"
 require "sign_up_test"
+require "tyrant/sign_up"
+
+User = Struct.new(:id, :auth_meta_data, :email) do
+  def save
+    @saved = true
+  end
+  def persisted?
+    @saved or false
+  end
+end
+
+class SignUpConfirmedTest < MiniTest::Spec
+  Authenticatable = Tyrant::Authenticatable
+  User = Struct.new(:auth_meta_data)
+end
 
 class ResetPasswordTest < MiniTest::Spec
   it do
@@ -15,6 +30,14 @@ class ResetPasswordTest < MiniTest::Spec
     assert Tyrant::Authenticatable.new(op.model).digest == "123123"
     Tyrant::Authenticatable.new(op.model).confirmed?.must_equal true
     Tyrant::Authenticatable.new(op.model).confirmable?.must_equal false
+
+    reset = Tyrant::ResetPassword.new()
+    newModel = reset.new_authentication(op.model)
+
+
+    assert Tyrant::Authenticatable.new(newModel).digest == "NewPassword"
+
+
   end
   
 end
