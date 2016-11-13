8520005bb740ff90f968a071c863996136aedac4
emaglio <emanuele.magliozzi@gmail.com>
2016-11-14 09:29:56 +1100

    Tyrant::ResetPassword as TRB::Operation

--- lib/tyrant/reset_password.rb
+++ lib/tyrant/reset_password.rb
@@ -6,22 +6,22 @@ require "reform/form/validate"
 require "reform/form/active_model/validations"
 
 module Tyrant
-  class ResetPassword
-    # include Model
-    # model User, :find
+  class ResetPassword < trailblazer::Operation
+    include Model
+    model User, :find
     
-    # def process(params)
-    #   new_authentication(model)
-    # end
+    def process(params)
+      new_authentication(model)
+      contract.save
+    end
 
-  # private
+  private
     def new_authentication(model)
       auth = Tyrant::Authenticatable.new(model)
       auth.digest!(generate_password) # contract.auth_meta_data.password_digest = ..
       auth.confirmed!
       auth.sync
       notify_user(model.email)
-      return model
     end
 
     def generate_password
