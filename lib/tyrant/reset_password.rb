d9022ffa25c7dabb68610103da31a18b17b312b9
emaglio <emanuele.magliozzi@gmail.com>
2016-11-16 11:38:19 +1100

    Fixed some issue when you sending real email

--- lib/tyrant/reset_password.rb
+++ lib/tyrant/reset_password.rb
@@ -18,6 +18,7 @@ module Tyrant
       model.save
     end
 
+  private
     def new_authentication
       auth = Tyrant::Authenticatable.new(model)
       new_password = generate_password
@@ -32,29 +33,31 @@ module Tyrant
 
     def notify(email, new_password)
       send_email = Tyrant::Mailer.new()
-      send_email = notify_user(email, new_password)
+      send_email.pony_options
+      send_email.notify_user(email, new_password)
     end
 
   end
 
   class Mailer 
-    def initialize
+    def pony_options
       Pony.options = {
-                from: "admin@email.com",
-                via: :smtp, 
-                via_options: {address: "smtp.gmail.com", 
-                port: "587",
-                domain: 'localhost:3000', 
-                enable_starttls_auto: true, 
-                user_name: "admin@email.com", 
-                password: "password", 
-                subject: "Reset password for your application",
-                authentication: :plain} 
-      }
+                      from: "admin@email.com",
+                      via: :smtp, 
+                      via_options: {
+                                    address: "smtp.gmail.com", 
+                                    port: "587",
+                                    domain: 'localhost:3000', 
+                                    enable_starttls_auto: true, 
+                                    user_name: "your_email@gmail.com", 
+                                    password: "your_password", 
+                                    subject: "Reset password for your application",
+                                    authentication: :plain
+                                    } 
+                      }
     end
     
     def notify_user(email, new_password)
-      Tyrant::Mailer.new()
       Pony.mail({ to: email,
                   body: "Hi there, here your temporary password: #{new_password}. We suggest you to modify this password ASAP. Cheers",
                 })
