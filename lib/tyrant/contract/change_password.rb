5802d94b36b00c4cde86b1bb1ccea36b2ac1453f
emaglio <emanuele.magliozzi@gmail.com>
2017-01-29 14:58:10 +1100

    Absolute path for error_messages file - not sure if correct

--- lib/tyrant/contract/change_password.rb
+++ lib/tyrant/contract/change_password.rb
@@ -12,7 +12,7 @@ module Tyrant::Contract
     validation do
       configure do
         option :form
-        config.messages_file = 'lib/config/error_messages.yml'
+        config.messages_file = '/home/emamaglio/projects/tyrant/lib/config/error_messages.yml' 
 
         def user_exists?
           User.where(email: form.email).size == 1
--- lib/tyrant/contract/sign_up.rb
+++ lib/tyrant/contract/sign_up.rb
@@ -11,7 +11,7 @@ module Tyrant::Contract
     validation do
       configure do
         option :form
-        config.messages_file = 'lib/config/error_messages.yml'
+        config.messages_file = '/home/emamaglio/projects/tyrant/lib/config/error_messages.yml'
 
         def unique_email?
           User.where("email = ?", form.email).size == 0
