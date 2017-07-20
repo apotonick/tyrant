af77066841a8f782148f1b74d946584374f21533
Nick Sutterer <apotonick@gmail.com>
2017-07-20 17:17:58 +0200

    inherit from Abstract.

--- lib/tyrant/sign_up/operation/confirmed.rb
+++ lib/tyrant/sign_up/operation/confirmed.rb
@@ -1,25 +1,7 @@
 module Tyrant
   class SignUp < Trailblazer::Operation
-    class Confirmed < Trailblazer::Operation
-      step Model( ::User, :new )
-      step Contract::Build( constant: Form::WithConfirmPassword )
-      step Contract::Validate()
-      step Contract::Persist( method: :sync ) # write :email to model.
-      step :digest!
-      step :save!
-
-      def digest!(options, model:, **)
-        auth = Tyrant::Authenticatable.new(model)
-
-        auth.digest!( options["contract.default"].password )
-        auth.confirmed!
-
-        auth.sync # write :auth_meta_data field to model.
-      end
-
-      def save!(options, model:, **)
-        model.save
-      end
+    class Confirmed < SignUp
+      step Contract::Build( constant: Form::WithConfirmPassword ), override: true
     end
   end
 end
